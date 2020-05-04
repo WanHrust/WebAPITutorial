fbacf65f2e157198f7bb42d916a1411d41368c7b
Rustam Alashrafov <rustam.alashrafov@gmail.com>
2020-05-04 12:36:27 +0200

    Add project files.

---
+++ WebAPITutorial/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITutorial.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this._courseLibraryRepository = courseLibraryRepository;
+        }
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
